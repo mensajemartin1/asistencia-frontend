2b4702400b3a1bcdf8578ffa8008b416697d5820
joseorteha <joseortegahac@gmail.com>
2026-03-19 00:15:45 -0600

    4: Corregir obtener_asistencia.php: actualizar JOINs a schema v3 y agregar LIMIT

--- modules/attendance/controllers/obtener_asistencia.php
+++ modules/attendance/controllers/obtener_asistencia.php
@@ -4,28 +4,31 @@ require_once __DIR__ . '/../../../config/database.php';
 $conn = getConnection();
 
 $sql = "SELECT
-    Asistencias.id,
-    Alumnos.matricula,
-    Alumnos.nombre     AS alumno,
-    Materias.nombre    AS materia,
-    Asistencias.estado,
-    Asistencias.fecha,
-    Asistencias.hora
-FROM Asistencias
-INNER JOIN Alumnos  ON Asistencias.idAlumno  = Alumnos.id
-INNER JOIN Materias ON Asistencias.idMateria = Materias.id
-ORDER BY Asistencias.id DESC";
+    a.id,
+    al.matricula,
+    al.nombre AS alumno,
+    m.nombre  AS materia,
+    a.estado,
+    a.fecha,
+    a.hora
+FROM Asistencias a
+INNER JOIN Alumnos al        ON a.idAlumno       = al.id
+INNER JOIN GruposMaterias gm ON a.idGrupoMateria = gm.id
+INNER JOIN Materias m        ON gm.idMateria     = m.id
+ORDER BY a.id DESC
+LIMIT 200";
 
 $result = $conn->query($sql) or die($conn->error);
 
 while ($row = $result->fetch_assoc()) {
+    $estado = ucfirst((string)$row['estado']);
     echo "<tr>
-        <td>" . $row['id']        . "</td>
+        <td>" . (int)$row['id'] . "</td>
         <td>" . htmlspecialchars($row['matricula']) . "</td>
         <td>" . htmlspecialchars($row['alumno'])    . "</td>
         <td>" . htmlspecialchars($row['materia'])   . "</td>
-        <td>" . htmlspecialchars($row['estado'])    . "</td>
-        <td>" . $row['fecha']     . "</td>
-        <td>" . $row['hora']      . "</td>
+        <td>" . htmlspecialchars($estado) . "</td>
+        <td>" . htmlspecialchars((string)$row['fecha']) . "</td>
+        <td>" . htmlspecialchars((string)$row['hora'])  . "</td>
     </tr>";
 }
